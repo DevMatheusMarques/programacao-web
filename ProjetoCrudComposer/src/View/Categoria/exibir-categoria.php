a4af87c377feeffe6ee283f3d8988bded84bf214
DevMatheusMarques <estudosfatecmhcm@gmail.com>
2024-06-04 00:02:09 -0300

    Ajustes de erros ao excluir e alterar dados

--- ProjetoCrudComposer/src/View/Categoria/exibir-categoria.php
+++ ProjetoCrudComposer/src/View/Categoria/exibir-categoria.php
@@ -44,7 +44,7 @@ document.addEventListener("DOMContentLoaded", function () {
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
     <!-- Icon -->
-    <link rel="icon" href="./../public/assets/book-solid.png" type="image/x-icon">
+    <link rel="icon" href="../../public/assets/book-solid.png" type="image/x-icon">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
